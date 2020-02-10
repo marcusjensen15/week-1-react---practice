c393d8aa4aa406c509e0e6ff95cccc2d834e91d9
Student <example@example.com>
2020-02-10 10:40:58 -0800

    basic react practice

---
+++ src/components/TicketList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Ticket from "./Ticket"
+
+
+function TicketList(){
+  return (<Ticket
+    location="10A"
+    names="Marcus & Tyler"
+    issue="Problems man"
+
+    />);
+  }
+
+  export default TicketList;
